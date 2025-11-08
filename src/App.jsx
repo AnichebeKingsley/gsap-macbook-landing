da40858589935751299ded817d2db13cc12e301c
AnichebeKingsley <kingsleyanichebe222@gmail.com>
2025-11-08 17:29:19 +0100

    made changes to code

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
 import Productviewer from './components/three/Productviewer.jsx'
 import gsap from 'gsap'
+import Showcase from './components/Showcase.jsx'
 import { ScrollTrigger, SplitText } from 'gsap/all'
 
 gsap.registerPlugin(ScrollTrigger,);
@@ -13,6 +14,7 @@ const App = () => {
      <Navbar />
      <Hero />
      <Productviewer />
+     <Showcase />
    </main>
   )
 }
